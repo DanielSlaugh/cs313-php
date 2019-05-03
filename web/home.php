b33d70fcf3a947f35b5e74e43e3b9c5742ac37b1
DanielSlaugh <sla15004@byui.edu>
2019-05-03 17:06:27 -0600

    donesky

--- web/home.php
+++ web/home.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Document</title>
-</head>
-<body>
-   <P>dis working??</p>
-</body>
+
+    <head>
+      <meta charset="utf-8">
+      <meta name="description" content="Slnik University is a private, four year university. Slink university seeks to create a wholesome learning environment in which students can strengthen their commitment to their faith and receive a quality education that prepares them for leadership in the home, the community, and the workplace.">
+      <meta name="viewpoint" content="width=device-width, initial-scale=1">
+      <link href="assign08Style.css" rel="stylesheet">
+      <link rel=canonical href="http://157.201.194.254/~dlslaug/assign08.html">
+      <title>Slink University</title>
+
+      <style>
+
+      </style>
+    </head>
+
+    <body>
+
+      <header>
+         <h1 style="padding-bottom: 30px; color: white;" >
+            <p>Slink
+            <p style="font-size: 50%">University</p>
+         </h1>
+
+       </header>
+
+         <div class="container">
+            <img src="campus.jpg" alt="campus students" style="width:100%;">
+            <div class="centered">
+               <ul>
+                 <li><a id="Top" href="assign08.html" style="color: white; text-decoration: none;">Home</a></li>
+                 <li><a href="assign08B.html" style="color: white; text-decoration: none;">Academics</a></li>
+                 <li><a href="assign08C.html" style="color: white; text-decoration: none;">Athletics</a></li>
+                 <li><a href="assign08D.html" style="color: white; text-decoration: none;">Fine Arts</a></li>
+                 <li><a href="http://www.byui.edu/" style="color: white; text-decoration: none;">Our Partner School</a></li>
+               </ul>
+            </div>
+            <nav class="top-left" style="color: black;">
+               <p>“Your comfort zone is a place where you keep yourself in a self-illusion and nothing can grow there but your potentiality can grow only when you can think and grow out of that zone.” <br> ― Rashedur Ryan Rahman</p>
+            </nav>
+         </div>
+
+         <footer>
+            <p>Daniel Slaugh</p>
+            <p>CS 213</p>
+            <p>Copyright 2019</p>
+         </footer>
+
+    </body>
 </html>
