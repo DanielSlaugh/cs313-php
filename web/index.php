01bc8a3fb077b1d391a3dcd17475b226203cce8e
DanielSlaugh <sla15004@byui.edu>
2019-05-03 17:15:42 -0600

    t

--- web/index.php
+++ web/index.php
@@ -15,25 +15,21 @@
    </head>
    <body>
       <div style="background-color: black; text-align: center;">
-         <h1 style=" text-align: center; color: lightgrey;">CS 213 Assignments</h1>
-         <p style="color: grey; font-size: 18px;">Daniel Slaugh - Software Engineer, Colorado Springs native, and food lover
-         <hr />
-         </p>
+         <h1 style=" text-align: center; color: lightgrey;">Daniel's 313 HW</h1>
 
-         <ul>
-            <li><a style="color: white;" href="index.html">CS 213 Assignments</a></li>
-            <li><a style="color: white;" href="assign02.html">Assignment 02</a></li>
-            <li><a style="color: white;" href="assign03.html">Assignment 03</a></li>
-            <li><a style="color: white;" href="assign04.html">Assignment 04</a></li>
-            <li><a style="color: white;" href="assign05.html">Assignment 05</a></li>
-            <li><a style="color: white;" href="assign06.html">Assignment 06</a></li>
-            <li><a style="color: white;" href="assign07.html">Assignment 07</a></li>
-            <li><a style="color: white;" href="assign08.html">Assignment 08</a></li>
-            <li><a style="color: white;" href="assign09.html">Assignment 09</a></li>
-            <li><a style="color: white;" href="assign10.html">Assignment 10</a></li>
-            <li><a style="color: white;" href="assign11.html">Assignment 11</a></li>
-            <li><a style="color: white;" href="assign12.html">Assignment 12</a></li>
-            <li><a style="color: white;" href="assign13.html">Assignment 13</a></li>
+         <ul class="container">
+            <li><a href="assign02.html">Assignment 02</a></li>
+            <li><a href="assign03.html">Assignment 03</a></li>
+            <li><a href="assign04.html">Assignment 04</a></li>
+            <li><a href="assign05.html">Assignment 05</a></li>
+            <li><a href="assign06.html">Assignment 06</a></li>
+            <li><a href="assign07.html">Assignment 07</a></li>
+            <li><a href="assign08.html">Assignment 08</a></li>
+            <li><a href="assign09.html">Assignment 09</a></li>
+            <li><a href="assign10.html">Assignment 10</a></li>
+            <li><a href="assign11.html">Assignment 11</a></li>
+            <li><a href="assign12.html">Assignment 12</a></li>
+            <li><a href="assign13.html">Assignment 13</a></li>
          </ul>
       </div>
 
