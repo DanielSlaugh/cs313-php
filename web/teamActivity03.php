6999192027067dd7ed9167445287d03c764d2772
DanielSlaugh <sla15004@byui.edu>
2019-05-07 16:01:32 -0600

    d

--- web/teamActivity03.php
+++ web/teamActivity03.php
@@ -1,30 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>Document</title>
 </head>
+
 <body>
-   <form>
+   <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
       <label for="Name">Name: </label>
-      <input type="text" name="Name">
+      <input type="text" name="name">
       <br>
       <label for="Email">Email: </label>
-      <input type="text" name="Email">
+      <input type="text" name="email">
       <br>
       <label for="major">Major:</label>
       <select id="major" name="major">
-      <br><br>
-      <option value="cs">Computer Science</option>
-      <option value="wwd">Web Design and Development</option>
-      <option value="cit">Computer Information tech</option>
-      <option value="ce">Computer engineering</option>
+         <br><br>
+         <option value="cs">Computer Science</option>
+         <option value="wwd">Web Design and Development</option>
+         <option value="cit">Computer Information tech</option>
+         <option value="ce">Computer engineering</option>
       </select>
       <br><br>
       <label for="comments" rows="5" cols="40">Comments:</label>
       <textarea name="comments"></textarea>
+      <button type="submit" value="submit" name="submit"></button>
    </form>
+
+   <?php
+   // Declare Variables
+   $name = "";
+   $email = "";
+   $major = "";
+   $comments = "";
+
+   $name = $_POST["name"];
+   $email = $_POST["email"];
+   $major = $_POST["major"];
+   $comments = $_POST["comments"];
+
+   ?>
+
+
+
 </body>
+
 </html>
