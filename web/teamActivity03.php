aafa8b1d953441709734ed5cd591bd9e10f03d6a
DanielSlaugh <sla15004@byui.edu>
2019-05-07 15:45:29 -0600

    b

---
+++ web/teamActivity03.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+</head>
+<body>
+   <form>
+      <input type="text" name="Name: ">
+      <input type="text" name="Email: ">
+      <label for="major">Major:</label>
+      <select id="major" name="major">
+      <option value="cs">Computer Science</option>
+      <option value="wwd">Web Design and Development</option>
+      <option value="cit">Computer Information tech</option>
+      <option value="ce">Computer engineering</option>
+      </select>
+      <label for="comments">Comments:</label>
+      <input type="text area" name="comments">
+   </form>
+</body>
+</html>
