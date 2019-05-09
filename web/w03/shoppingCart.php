8cb66201839f4d42de680899a3d117edea57a48d
DanielSlaugh <sla15004@byui.edu>
2019-05-09 14:22:26 -0600

    n

--- web/w03/shoppingCart.php
+++ web/w03/shoppingCart.php
@@ -13,7 +13,36 @@
 </head>
 
 <body>
-   <h1>Hello, world!</h1>
+   <header>
+      <h1 style="padding-bottom: 30px; color: white;">
+         <p>Daniel Slaugh
+            <p style="font-size: 50%">Home Page</p>
+      </h1>
+   </header>
+   <div class="container">
+      <img src="whitney.jpg" alt="campus students" style="width:100%;">
+      <div class="centered">
+         <ul>
+            <li><a id="Top" href="https://polar-plateau-20469.herokuapp.com/home.php" style="color: white; text-decoration: none;">Home</a></li>
+            <li><a href="https://polar-plateau-20469.herokuapp.com/index.php" style="color: white; text-decoration: none;">More Websites</a></li>
+            <li><a href="https://www.tripadvisor.com/Restaurants-g35583-Rexburg_Idaho.html" style="color: white; text-decoration: none;">Food</a></li>
+            <li><a href="http://www.byui.edu/canvas-authentication" style="color: white; text-decoration: none;">Canvas</a></li>
+            <li><a href="http://www.byui.edu/" style="color: white; text-decoration: none;">BYUI Home</a></li>
+         </ul>
+      </div>
+      <nav class="top-left" style="color: black;">
+         <?php
+         print "<p>“Don't be pushed around by the fears in your mind.
+                 Be lead by the dreams in your heart.” <br> ― Roy T. Bennett</p>"
+         ?>
+      </nav>
+   </div>
+
+   <footer>
+      <p>Daniel Slaugh</p>
+      <p>CS 313</p>
+      <p>Copyright 2019</p>
+   </footer>
 
    <!-- Optional JavaScript -->
    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
