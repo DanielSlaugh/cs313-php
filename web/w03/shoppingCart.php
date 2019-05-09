253bc16975f8a2bc8dcc18425b30a324762a8e16
DanielSlaugh <sla15004@byui.edu>
2019-05-09 15:07:52 -0600

    b

--- web/w03/shoppingCart.php
+++ web/w03/shoppingCart.php
@@ -23,11 +23,11 @@
 
       <div class="centered">
          <ul style="width: 100%">
-            <li><a id="Top" href="https://polar-plateau-20469.herokuapp.com/home.php" style="color: white; text-decoration: none;">Home</a></li>
-            <li><a href="https://polar-plateau-20469.herokuapp.com/index.php" style="color: white; text-decoration: none;">More Websites</a></li>
-            <li><a href="https://www.tripadvisor.com/Restaurants-g35583-Rexburg_Idaho.html" style="color: white; text-decoration: none;">Food</a></li>
-            <li><a href="http://www.byui.edu/canvas-authentication" style="color: white; text-decoration: none;">Canvas</a></li>
-            <li><a href="http://www.byui.edu/" style="color: white; text-decoration: none;">BYUI Home</a></li>
+            <li><a id="Top" href="https://polar-plateau-20469.herokuapp.com/home.php" >Home</a></li>
+            <li><a href="https://polar-plateau-20469.herokuapp.com/index.php" >More Websites</a></li>
+            <li><a href="https://www.tripadvisor.com/Restaurants-g35583-Rexburg_Idaho.html" >Food</a></li>
+            <li><a href="http://www.byui.edu/canvas-authentication" >Canvas</a></li>
+            <li><a href="http://www.byui.edu/" >BYUI Home</a></li>
          </ul>
       </div>
       <nav class="top-left" style="color: black;">
