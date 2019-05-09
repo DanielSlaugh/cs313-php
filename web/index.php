4d8cab08deb755ba1866d7001f9ffc914ff80750
DanielSlaugh <sla15004@byui.edu>
2019-05-09 14:16:10 -0600

    j

--- web/index.php
+++ web/index.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <!--Index for all of the CS 213 Assignemts this semester-->
 
-<html lang = "en">
-   <head>
-      <meta charset="utf-8">
-      <meta name="viewpoint" content="width=device-width, initial-scale=1">
-      <link href="home.css" rel="stylesheet">
-      <title>Daniel Slaugh's Assignments</title>
-   </head>
-   <body>
-         <h1 style=" text-align: center; color: lightgrey;">Daniel's 313 HW</h1>
-         <section class="container">
-            <li><a href="https://polar-plateau-20469.herokuapp.com/home.php">Assignment 02</a></li>
-            <li><a href="assign03.html">Assignment 03</a></li>
-            <li><a href="assign04.html">Assignment 04</a></li>
-            <li><a href="assign05.html">Assignment 05</a></li>
-            <li><a href="assign06.html">Assignment 06</a></li>
-            <li><a href="assign07.html">Assignment 07</a></li>
-            <li><a href="assign08.html">Assignment 08</a></li>
-            <li><a href="assign09.html">Assignment 09</a></li>
-            <li><a href="assign10.html">Assignment 10</a></li>
-            <li><a href="assign11.html">Assignment 11</a></li>
-            <li><a href="assign12.html">Assignment 12</a></li>
-            <li><a href="assign13.html">Assignment 13</a></li>
-         </section>
-      </div>
-   </body>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewpoint" content="width=device-width, initial-scale=1">
+   <link href="home.css" rel="stylesheet">
+   <title>Daniel Slaugh's Assignments</title>
+</head>
+
+<body>
+   <h1 style=" text-align: center; color: lightgrey;">Daniel's 313 HW</h1>
+   <section class="container">
+      <li><a href="https://polar-plateau-20469.herokuapp.com/home.php">Assignment 02</a></li>
+      <li><a href="https://polar-plateau-20469.herokuapp.com/w03/shoppingCart.php">Assignment 03</a></li>
+      <li><a href="assign04.html">Assignment 04</a></li>
+      <li><a href="assign05.html">Assignment 05</a></li>
+      <li><a href="assign06.html">Assignment 06</a></li>
+      <li><a href="assign07.html">Assignment 07</a></li>
+      <li><a href="assign08.html">Assignment 08</a></li>
+      <li><a href="assign09.html">Assignment 09</a></li>
+      <li><a href="assign10.html">Assignment 10</a></li>
+      <li><a href="assign11.html">Assignment 11</a></li>
+      <li><a href="assign12.html">Assignment 12</a></li>
+      <li><a href="assign13.html">Assignment 13</a></li>
+   </section>
+   </div>
+</body>
+
 </html>
 </body>
+
 </html>
 
 <?php
