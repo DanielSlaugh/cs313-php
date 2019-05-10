ec2077e4d66344b6523e77b7e814e9460785a454
DanielSlaugh <sla15004@byui.edu>
2019-05-09 21:50:21 -0600

    b

--- web/w03/shoppingCart.php
+++ web/w03/shoppingCart.php
@@ -30,13 +30,15 @@
          <p>Shopping Cart</p>
       </h1>
    </header>
-   <ul style="width: 100%" class="navbar-default">
-      <li><a id="Top" href="https://polar-plateau-20469.herokuapp.com/home.php">Home</a></li>
-      <li><a href="https://polar-plateau-20469.herokuapp.com/index.php">More Websites</a></li>
-      <li><a href="https://www.tripadvisor.com/Restaurants-g35583-Rexburg_Idaho.html">Food</a></li>
-      <li><a href="http://www.byui.edu/canvas-authentication">Canvas</a></li>
-      <li><a href="http://www.byui.edu/">BYUI Home</a></li>
-   </ul>
+   <div class="navbar-default">
+      <ul style="width: 100%" class="active">
+         <li><a id="Top" href="https://polar-plateau-20469.herokuapp.com/home.php">Home</a></li>
+         <li><a href="https://polar-plateau-20469.herokuapp.com/index.php">More Websites</a></li>
+         <li><a href="https://www.tripadvisor.com/Restaurants-g35583-Rexburg_Idaho.html">Food</a></li>
+         <li><a href="http://www.byui.edu/canvas-authentication">Canvas</a></li>
+         <li><a href="http://www.byui.edu/">BYUI Home</a></li>
+      </ul>
+   </div>
 
    <div class="top-right">
       <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
