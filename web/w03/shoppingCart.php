0f631b6a08d5751edb8d0ef7f5260f454f27fd7a
DanielSlaugh <sla15004@byui.edu>
2019-05-09 19:59:38 -0600

    b

--- web/w03/shoppingCart.php
+++ web/w03/shoppingCart.php
@@ -47,12 +47,6 @@
       <input type="image" name="wrench" src="wrench.jpg" alt="wrench" style="width:15%;">
    </div>
 
-   <footer class="bottom-left">
-      <p>Daniel Slaugh</p>
-      <p>CS 313</p>
-      <p>Copyright 2019</p>
-   </footer>
-
    <!-- Optional JavaScript -->
    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
