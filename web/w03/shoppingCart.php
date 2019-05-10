74fe21db2ffb74752c469ec1719b9600df847383
DanielSlaugh <sla15004@byui.edu>
2019-05-10 12:13:39 -0600

    n

--- web/w03/shoppingCart.php
+++ web/w03/shoppingCart.php
@@ -45,7 +45,9 @@
          <table>
             <tr>
                <td><input type="image" name="broccoli" src="broccoli.jpg" alt="broc" style="width:15%;">
-                  <input id="check01" type="checkbox" name="checklist[]" value="broccoli" onclick="addItem(document.getElementById("check01"));" </td> </tr> <br>
+                  <input id="check01" type="checkbox" name="checklist[]" value="broccoli" onclick="addItem(document.getElementById("check01"))">
+                </td>
+            </tr> <br>
             <tr>
                <td><input type="image" name="canta" src="canta.jpg" alt="cant" style="width:15%;">
                   <input id="check02" type="checkbox" name="checklist[]" value="cantalope"></td>
