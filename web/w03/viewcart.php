6c10bdfa7516d36a5ddc4b2b4f05699596ce8401
DanielSlaugh <sla15004@byui.edu>
2019-05-10 18:17:41 -0600

    n

--- web/w03/viewcart.php
+++ web/w03/viewcart.php
@@ -1,21 +1,25 @@
 <?php
-   session_start();
+session_start();
 
-   $broccoli = $_SESSION["broccoli"];
+// $broccoli = $_SESSION["broccoli"];
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=1, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>View Cart</title>
 </head>
+
 <body>
-<h5>Here are your items: <?php echo $broccoli; ?></h5>
+   <h5>Here are your items:</h5>
+   <h5><?php foreach ($_SESSION as $key => $val){echo $key . " " . $val . "<br/>";} ?></h5>
 </body>
+
 </html>
 
 
