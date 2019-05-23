16b59f8394471e880c6dde094e4f3415543bb849
DanielSlaugh <sla15004@byui.edu>
2019-05-22 20:43:52 -0600

    n

--- web/fakebook/home.php
+++ web/fakebook/home.php
@@ -53,4 +53,50 @@
 
 </body>
 
+<script>
+   function search(text) {
+      let url = "teamActivity05.php?search=" + text;
+      window.location = url;
+   }
+</script>
+
 </html>
+
+<?php
+try {
+   $dbUrl = getenv('DATABASE_URL');
+
+   $dbOpts = parse_url($dbUrl);
+
+   $dbHost = $dbOpts["host"];
+   $dbPort = $dbOpts["port"];
+   $dbUser = $dbOpts["user"];
+   $dbPassword = $dbOpts["pass"];
+   $dbName = ltrim($dbOpts["path"], '/');
+
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $ex) {
+   echo 'Error!: ' . $ex->getMessage();
+   die();
+}
+
+if (isset($_GET['search'])) {
+   $stmt = $db->prepare('SELECT book, chapter, verse, content FROM message WHERE book=:book');
+   $stmt->execute(array(':book' => $_GET['search']));
+   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+   foreach ($rows as $message) {
+      echo "<b>" . $message['book'] . " " . $message['chapter'] . ": " . $message['verse'] . "</b> - " . $message['content'];
+      echo '<br/>';
+   }
+} else {
+   foreach ($db->query('SELECT book, chapter, verse, content FROM message') as $message) {
+      echo "<b>" . $message['book'] . " " . $message['chapter'] . ": " . $message['verse'] . "</b> - " . $message['content'];
+      echo '<br/>';
+   }
+}
+
+?>
