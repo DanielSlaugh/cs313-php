86778192893c78daf6b8add985fdac58501f5873
DanielSlaugh <sla15004@byui.edu>
2019-05-29 15:45:09 -0600

    m

--- web/fakebook/home.php
+++ web/fakebook/home.php
@@ -94,7 +94,7 @@ $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             <form class="full_width" id="login_form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                <label for="uname"><b>Username</b></label><br>
-               <input type="text" placeholder="Enter Username" name="uname" required>
+               <input type="text" placeholder="Enter Username" name="uname" required><br>
                <label for="psw"><b>Password</b></label><br>
                <input type="password" placeholder="Enter Password" name="psw" required><br>
                <button class="auth_button" type="submit">Login</button>
