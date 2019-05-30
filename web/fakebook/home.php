772a1b21473c4fb5e389906deb05e957831c2a9e
DanielSlaugh <sla15004@byui.edu>
2019-05-29 21:14:26 -0600

    b

--- web/fakebook/home.php
+++ web/fakebook/home.php
@@ -9,9 +9,7 @@ $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $username = htmlspecialchars($_POST['uname']);
 $user_password = htmlspecialchars($_POST['psw']);
 
-$query = 'SELECT u.display_name, m.message_text, m.message_time FROM users u JOIN message m ON u.id = m.user_id';
-
-// $query = 'SELECT u.username, u.password, u.display_name, m.message_text, m.message_time, FROM users u WHERE u.username=$fusername AND u.password=$user_password JOIN message m ON u.id = m.user_id';
+$query = 'SELECT password, display_name, FROM users WHERE username=$fusername AND u.password=$user_password';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $current_user = $stmt->fetchAll(PDO::FETCH_ASSOC);
