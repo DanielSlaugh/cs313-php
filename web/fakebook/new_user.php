62580fdabe69d1e24db9f67b8c94936d6bea0262
DanielSlaugh <sla15004@byui.edu>
2019-05-29 18:59:15 -0600

    n

--- web/fakebook/new_user.php
+++ web/fakebook/new_user.php
@@ -12,14 +12,25 @@ require('dbconnect.php');
 $db = get_db();
 
 $stmt = $db->prepare('INSERT INTO users (username, password, display_name) VALUES (:username, :user_password, :display_name);');
+echo "1";
+echo "<br>";
+
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+echo "2";
+echo "<br>";
 $stmt->bindValue(':user_password', $user_password, PDO::PARAM_STR);
+echo "3";
+echo "<br>";
 $stmt->bindValue(':display_name', $display_name, PDO::PARAM_STR);
+echo "4";
+echo "<br>";
 $stmt->execute();
 
 $new_page = "home.php";
 
 header("Location: $new_page");
+echo "5";
+echo "<br>";
 die();
 
 ?>
