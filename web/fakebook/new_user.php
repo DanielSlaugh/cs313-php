e81d6d117aa506e408e332fa708c277d3357cda3
DanielSlaugh <sla15004@byui.edu>
2019-05-29 18:35:48 -0600

    f

--- web/fakebook/new_user.php
+++ web/fakebook/new_user.php
@@ -1,11 +1,25 @@
 <?php
 
-$user_name = htmlspecialchars($_POST['uname']);
+$username = htmlspecialchars($_POST['uname']);
 $user_password = htmlspecialchars($_POST['psw']);
 $display_name = htmlspecialchars($_POST['dname']);
 
-echo $user_name;
+echo $username;
 echo $user_password;
 echo $display_name;
+
+require('dbconnect.php');
+$db = get_db();
+
+$stmt = $db->prepare('INSERT INTO users (username, password, display_name) VALUES (:username, :user_password, :display_name);');
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+$stmt->bindValue(':user_password', $user_password, PDO::PARAM_STR);
+$stmt->bindValue(':display_name', $display_name, PDO::PARAM_STR);
+$stmt->execute();
+
+$new_page = "home.php";
+
+header("Location: $new_page");
+die();
 
 ?>
