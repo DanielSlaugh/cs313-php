9c97c9df835d1c51707db95d335197784832d8b0
DanielSlaugh <sla15004@byui.edu>
2019-06-01 13:31:22 -0600

    m

--- web/fakebook/home.php
+++ web/fakebook/home.php
@@ -2,26 +2,27 @@
 require('dbConnect.php');
 $db = get_db();
 
+// Get all the variables to load message feed when the home page is created
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-// $query = 'SELECT u.password, u.username, u.display_name, m.message_text, m.message_time FROM users u JOIN message m ON u.id = m.user_id';
 $query = 'SELECT u.display_name, m.message_text, m.message_time FROM users u JOIN message m ON u.id = m.user_id ORDER BY m.message_time DESC';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// These variables need a global scope to be accessed in the HTML
 $username = "";
 $user_password = "";
 $valid_user = false;
 $current_display_name = "";
 
+// if the user has tried to log in, check what they types against the database
+// If they're good to go, save their data to work with
 if (isset($_POST['uname'])) {
    $username = htmlspecialchars($_POST['uname']);
-   // echo $username;
 }
 if (isset($_POST['psw'])) {
    $user_password = htmlspecialchars($_POST['psw']);
-   // echo $user_password;
    try {
       $stmt = $db->prepare("SELECT u.id, u.display_name, u.username, u.password FROM users u WHERE u.username='$username' AND u.password='$user_password'");
       $stmt->execute();
@@ -30,21 +31,14 @@ if (isset($_POST['psw'])) {
       echo $e->getMessage();
    }
    foreach ($current_user as $user) {
-      // This variable will only get set if we have a valid entry in the table
-      // A valid username and password!
+      // valid_user will only get set if we have a valid entry in the table
       $valid_user = true;
    }
-
    foreach ($current_user as $user) {
       $current_display_name = $user['display_name'];
       $current_id = $user['id'];
    }
 }
-
-// if we don't have a login, go to the log in page
-// else if we do, ,
-// if the username and password are real, load the page with variables
-// else, go to the login page and display error message
 
 ?>
 
@@ -66,6 +60,7 @@ if (isset($_POST['psw'])) {
 
    <div class="app-wrap">
 
+      <!-- Top Menu bar -->
       <div class="icon-bar">
          <a><i class="fa fa-home" onclick="load_home_page(val)"></i>Home</a>
          <a><i class="fa fa-bell"></i>Notifications</a>
@@ -73,6 +68,7 @@ if (isset($_POST['psw'])) {
          <a><i class="fa fa-user" onclick="load_profile_page()"></i>Me</a>
       </div>
 
+      <!-- What's on your mind user bar -->
       <header class="app-header" id="main_head">
          <a class="button" id="main_head"><i><?php if ($valid_user) {
                                                 echo $current_display_name;
@@ -83,6 +79,7 @@ if (isset($_POST['psw'])) {
          <a class="button" id="main_head" onclick="add_content()"><i class="fa fa-cog"></i></a>
       </header>
 
+      <!-- Message Feed -->
       <div class="content">
          <div id="content_parent">
             <ul class="posts" id="content">
@@ -90,7 +87,6 @@ if (isset($_POST['psw'])) {
                <?php
                foreach ($posts as $post) {
                   $dispay_name = $post['display_name'];
-                  // $user_id = $post['user_id'];
 
                   $time_day = substr($post['message_time'], 8, 2);
                   $time_month = substr($post['message_time'], 5, 2);
@@ -110,6 +106,7 @@ if (isset($_POST['psw'])) {
                ?>
             </ul>
 
+            <!-- Create a new Message -->
             <form id="form" action="">
                <input type="hidden" name="current_id" id="current_id" value="<?php echo $current_id ?>">
                <textarea class="textarea" id="message_text" cols="30" rows="10" placeholder="What's on your mind?" name="message_text"></textarea>
@@ -117,9 +114,10 @@ if (isset($_POST['psw'])) {
                                                                                      document.getElementById('message_text').value);
                                                                                      load_home_page(val);
                                                                                      window.location.reload();">
+               <br><br><br><br><br><br><br><br><br><br><br><br>
             </form>
 
-
+            <!-- Loggin Page -->
             <form class="full_width" id="login_form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                <label for="uname"><b>Username</b></label><br>
                <input class="full_width" type="text" placeholder="Enter Username" name="uname" required><br>
@@ -128,8 +126,10 @@ if (isset($_POST['psw'])) {
                <button class="auth_button" type="submit">Login</button>
                <p class="full_width">or</p>
                <button class="auth_button2" type="button" onclick="load_sign_up_page()">Sign-up</button>
+               <br><br><br><br><br><br><br>
             </form>
 
+            <!-- Sign up Page -->
             <form class="full_width" id="sign_up_form" method="post" action="new_user.php">
                <label for="uname"><b>Username</b></label><br>
                <input class="full_width" type="text" placeholder="Enter Username" name="uname" required><br>
@@ -140,14 +140,17 @@ if (isset($_POST['psw'])) {
                <button class="auth_button" type="submit">Sign-up</button>
                <p class="full_width">or</p>
                <button class="auth_button2" type="button" onclick="load_profile_page()">Login</button>
+               <br><br><br><br><br><br><br>
             </form>
 
+            <!-- Confirmation of new Account -->
             <p id="new_user_message"><br>Congratulations! You've created a new account. <br>
-               Go ahead and log in so you can have fun with your friends!</p>
+               Go ahead and log in so you can have fun with your friends!
+               <br><br><br><br><br><br><br><br><br><br>
+               <br><br></p>
 
          </div>
-         <br><br><br><br><br><br><br><br><br><br>
-         <br><br>
+
          <section id="spacing"></section>
       </div>
    </div>
